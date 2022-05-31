63c877aaa5ed506bab2d2bcee5026d61a9e2d608
batal.marklester@gmail.com <batal.marklester@gmail.com>
2022-06-01 03:39:13 +0800

    Day 1 Added: Splash Screen with animation and Login Page

--- app/src/main/java/com/example/batalfirebasefinalsproject/MainActivity.java
+++ app/src/main/java/com/example/batalfirebasefinalsproject/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.batalfirebasefinalsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    Animation bott_anim;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, Login.class);
+                finish();
+                startActivity(home);
+            }
+        }, 3000);
+
+        //Animations
+        bott_anim = AnimationUtils.loadAnimation(this,R.anim.splash_anim);
+
+        refs();
+
+        //Assign
+        logo.setAnimation(bott_anim);
     }
+    //Hooks
+    public void refs(){
+            logo = findViewById(R.id.splash_logo);
+        }
+
 }
