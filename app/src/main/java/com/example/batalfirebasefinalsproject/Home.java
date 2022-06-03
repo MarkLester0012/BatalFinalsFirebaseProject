e105214f4cd9c1c233fa43315332415ec1739955
batal.marklester@gmail.com <batal.marklester@gmail.com>
2022-06-04 01:36:21 +0800

    Day 2 Added: Home Page Edited: Wallpapers for the pages

---
+++ app/src/main/java/com/example/batalfirebasefinalsproject/Home.java
@@ -0,0 +1,19 @@
+package com.example.batalfirebasefinalsproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+
+    }
+
+}
